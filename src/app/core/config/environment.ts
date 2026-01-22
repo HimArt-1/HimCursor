21f9d74f9e9a962f7b7dcece294134caa4dc8b7e
HimArt-1 <t9an10@gmail.com>
2026-01-22 13:05:20 +0300

    Update environment.ts

--- src/app/core/config/environment.ts
+++ src/app/core/config/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    supabaseUrl: import.meta.env['VITE_SUPABASE_URL'] || 'https://pimodqiufnxqwgmewyse.supabase.co',
-    supabaseKey: import.meta.env['VITE_SUPABASE_KEY'] || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpbW9kcWl1Zm54cXdnbWV3eXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0NzcyNjgsImV4cCI6MjA4NDA1MzI2OH0.wooxuTrbTBKCFHlAKLLTKbGlJgeTh_Zj-h1cl6b1_Ec',
-    apiKey: import.meta.env['VITE_GEMINI_API_KEY'] || 'AIzaSyBIxbW23SUltiHlAYRj8mWldBDRX3mXc0U'
+    supabaseUrl: import.meta.env['VITE_SUPABASE_URL'] || 'https://شثقلابشثقلـثقل.supabase.co',
+    supabaseKey: import.meta.env['VITE_SUPABASE_KEY'] || 'eyسقفظياشثقياـثياـثقلا',
+    apiKey: import.meta.env['VITE_GEMINI_API_KEY'] || 'AIzشـسلـصسلـسلزـسيزد'
 };
